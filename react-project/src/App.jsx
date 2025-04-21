f5c1a672d1aeae8d7b3bfaf8e0b3bab1b12dde7b
rickardm57 <mrickard4@ivytech.edu>
2025-04-21 17:52:50 +0000

    commit;

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -61,6 +61,10 @@ function App() {
     (status) => !status,
     true
   );
+    useEffect(()=> {
+     console.log(`The restaurant status is ${status ? "open" : "closed"}`
+     ); 
+    }, [status]);
   return (
     <div>
       <h1>
